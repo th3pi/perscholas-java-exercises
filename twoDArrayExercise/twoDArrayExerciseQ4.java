a285894d588c1ba0ca7b80e2e4b200604a2d7da4
th3pi <38303217+th3pi@users.noreply.github.com>
2018-07-18 16:50:04 -0400

    added useful comments

--- twoDArrayExercise/twoDArrayExerciseQ4.java
+++ twoDArrayExercise/twoDArrayExerciseQ4.java
@@ -9,50 +9,64 @@ import java.util.Scanner;
  */
 public class twoDArrayExerciseQ4 {
 	public static void main(String[] args) {
+		
+		
 		Scanner kb = new Scanner(System.in);
 		char[][] map = new char[5][5];
+		
+		//Setting up the map with ground
 		for(int i = 0; i < map.length; i++) {
 			for(int j = 0; j < map[0].length; j++) {
 				map[i][j]='0';
 			}
 		}
+		
+		//Default value of the avatar
 		int row = 0, col = 0;
 		map[row][col] = 'X';
+		
+		//Display map for the first time
 		displayMap(map);
+		
+		
+		//Forever do-while loop that keeps on going until the user exits map (goes out of bounds)
 		do{System.out.println("Where would you like to move? You have 4 options: \n"
 				+ "1. Up \n"
 				+ "2. Down \n"
 				+ "3. Right \n"
 				+ "4. Left \n");
 			String s = kb.nextLine();
+			//What happens if user goes up
 			if(s.equalsIgnoreCase("Up")) {
-				map[row][col] = '0';
-				row--;
-				checkNegative(row);
-				map[row][col] = 'X';
-				displayMap(map);
+				map[row][col] = '0';	//Avatar's last location becomes empty
+				row--;					//Row reduces by one
+				checkBounds(row);		//Checks if avatar is inside bounds
+				map[row][col] = 'X';	//Avatar's new position
+				displayMap(map);		//Displays updated map
 			}else if(s.equalsIgnoreCase("Down")) {
-				map[row][col] = '0';
+				map[row][col] = '0';	
 				row++;
-				checkNegative(row);
+				checkBounds(row);
 				map[row][col] = 'X';
 				displayMap(map);
 			}else if(s.equalsIgnoreCase("Right")) {
 				map[row][col] = '0';
 				col++;
-				checkNegative(col);
+				checkBounds(col);
 				map[row][col] = 'X';
 				displayMap(map);
 			}else if(s.equalsIgnoreCase("Left")) {
 				map[row][col] = '0';
 				col--;
-				checkNegative(col);
+				checkBounds(col);
 				map[row][col] = 'X';
 				displayMap(map);
 			}
-		}while(true);
+		}while(true);		//While true means the loop will go on forever
 	}
 	
+	
+	//Method to display the map
 	static void displayMap(char[][] map) {
 		System.out.println("Current map: \n"
 				+ "==============");
@@ -63,7 +77,9 @@ public class twoDArrayExerciseQ4 {
 			System.out.println();
 		}
 	}
-	static void checkNegative(int i) {
+	
+	//Method to check if the avatar is going out of the map
+	static void checkBounds(int i) {
 		if(i < 0) {
 			System.out.println("You just exitted the map. Closing game.");
 			System.exit(0);
